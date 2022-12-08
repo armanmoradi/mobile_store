7fc6dd726eeeb674124dc6ca1284ade56d8bfe31
Arman Moradi <armanmoradi8gmail.com>
2022-12-08 21:54:43 +0330

    create view page for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products, only: [:show, :index, :new, :create]
   root 'products#index', to: 'index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
