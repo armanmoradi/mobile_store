c291b2953bc40079facda4dd1b75b3ce57530693
Arman Moradi <armanmoradi8gmail.com>
2022-12-08 20:59:05 +0330

    create model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,18 @@
 class ProductsController < ApplicationController
   def index
+    @product = Product.all
   end
 
   def new
   end
 
   def create
+    @product = Product.new(params.require(:product).permit(:name, :description))
+    @product.save
+    redirect_to @product
   end
 
   def show
-
+    
   end
 end
