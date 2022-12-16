f2bc959d4cf63cbefce0b36f1b72cca9f2ec0456
Arman Moradi <armanmoradi8gmail.com>
2022-12-17 01:04:12 +0330

    Add homepage styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'about', to: 'products#about'
-  root 'products#index', to: 'index'
+  get 'home', to: 'products#home'
+  root 'products#home', to: 'home'
   resources :products
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
